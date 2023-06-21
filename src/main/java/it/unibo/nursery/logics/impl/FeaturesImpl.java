7fe1d4aa43ed77966c3847d171da9cd2b7034940
Coli Alessandro <Alessandro.coli2@studio.unibo.it>
2023-06-21 15:46:52 +0200

    [Logics] addEmployee and addSupplier

--- src/main/java/it/unibo/nursery/logics/impl/FeaturesImpl.java
+++ src/main/java/it/unibo/nursery/logics/impl/FeaturesImpl.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
-
+import java.util.List;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 
@@ -19,7 +19,7 @@ public class FeaturesImpl implements Features {
 
     public FeaturesImpl(){
         String username = "root";
-        String password = "";
+        String password = "Delta_Velorum1";
 
         String dbName = "plantnursery";
         ConnectionProvider prov = new ConnectionProvider( username, password, dbName);
@@ -35,25 +35,32 @@ public class FeaturesImpl implements Features {
             statement.setString(3, CF);
             statement.setFloat(4,income);
             statement.setDate(5, Utils.dateToSqlDate(employment_date));
+            statement.setInt(6,this.getNext("Impiegati","id_imp"));
             statement.executeUpdate();
         } catch (final SQLIntegrityConstraintViolationException e) {
             throw new IllegalArgumentException(e);
         } catch (final SQLException e) {
             throw new IllegalStateException(e);
         }
-        throw new UnsupportedOperationException("Unimplemented method 'addEmployee'");
     }
 
     @Override
-    public void addSupplier() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addSupplier'");
+    public void addSupplier(String name) {
+        final String query = "INSERT INTO Fornitore (id_fornitore, nome) VALUES (?,?)";
+        try (final PreparedStatement statement = this.connection.prepareStatement(query)) {
+            statement.setInt(1,this.getNext("Fornitore","id_fornitore"));
+            statement.setString(2, name);
+            statement.executeUpdate();
+        } catch (final SQLIntegrityConstraintViolationException e) {
+            throw new IllegalArgumentException(e);
+        } catch (final SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
-    public void processInvoice() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'processInvoice'");
+    public void processInvoice(Date date, int id_imp, List<Integer> prod) {
+        
     }
 
     @Override
@@ -116,4 +123,20 @@ public class FeaturesImpl implements Features {
         throw new UnsupportedOperationException("Unimplemented method 'viewMoreTreated'");
     }
     
+
+    private int getNext(String table_name, String column) {
+        String query = "SELECT MAX("+ column + ") FROM " + table_name;
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            if (resultSet.next()) {
+                return resultSet.getInt(column) + 1;
+            } else {
+                return 1;
+            }
+        } catch (final SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
 }
