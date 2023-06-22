36807a3d193c01f95830200c0ed81175db51d47d
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2023-06-22 15:26:48 +0200

    [view] view part updated

--- src/main/java/it/unibo/nursery/view/impl/ApplicationController.java
+++ src/main/java/it/unibo/nursery/view/impl/ApplicationController.java
@@ -1,71 +1,110 @@
 package it.unibo.nursery.view.impl;
 
-import java.util.Date;
-import java.util.List;
-
-import javax.xml.catalog.CatalogFeatures.Feature;
-
 import it.unibo.nursery.logics.api.Features;
 import it.unibo.nursery.logics.impl.FeaturesImpl;
 import it.unibo.nursery.utils.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ApplicationController {
 
-    private FxAppView view;
-
-    private Features logics = new FeaturesImpl();
-
-    @FXML
-    private TextField nome_fornitore_add;
-    @FXML
-    private Button add_fornitore;
-    @FXML
-    private TextField nome_employee_add;
-    @FXML
-    private TextField cognome_employee_add;
-    @FXML
-    private TextField CF_employee_add;
-    @FXML
-    private TextField income_employee_add;
-    @FXML
-    private TextField hire_date;
-    @FXML
-    private Button add_employee;
-
-    public ApplicationController(FxAppView view) {
-        this.view = view;
-    }
-
-    @FXML
-    void addFornitoreOnClick(ActionEvent event) {
-        logics.addSupplier(nome_fornitore_add.getText());
-        nome_fornitore_add.clear();
-    }
+    @FXML private TextArea EmployeeView;
+    @FXML private TextArea StatView;
+    @FXML private Button add_employee;
+    @FXML private Button add_fornitore;
+    @FXML private TextField dateTreatment;
+    @FXML private TextField discount;
+    @FXML private TextField employeeCF;
+    @FXML private TextField employeeID;
+    @FXML private TextField employeeName;
+    @FXML private TextField employeeSalary;
+    @FXML private TextField employeeSurname;
+    @FXML private CheckBox fertilizer;
+    @FXML private TextField hireDate;
+    @FXML private TextField plantID;
+    @FXML private TextField plantTypeID;
+    @FXML private TextField shiftDate;
+    @FXML private TextField shiftEndTime;
+    @FXML private TextField shiftStartingTime;
+    @FXML private TextField statEndDate;
+    @FXML private TextField statStartingDate;
+    @FXML private TextField supplierID;
+    @FXML private TextField supplierName;
     
-    @FXML 
-    void addEmployeeOnClick(ActionEvent event){
-        logics.addEmployee(
-            nome_employee_add.getText(),
-            cognome_employee_add.getText(),
-            CF_employee_add.getText(), 
-            Float.parseFloat(income_employee_add.getText()),
-            Utils.buildDate(hire_date.getText()).orElse(null) ); //TODO in else metti oggi
-        nome_employee_add.clear();
-        cognome_employee_add.clear();
-        CF_employee_add.clear();
-        income_employee_add.clear();
-        hire_date.clear();
+    private Features features;
+
+    public ApplicationController() {
+        features = new FeaturesImpl();
     }
+
     @FXML
-    void processInvoice(ActionEvent event){
+    void addEmployeeOnClick(ActionEvent event) {
+        features.addEmployee(
+            employeeName.getText(),
+            employeeSurname.getText(),
+            employeeCF.getText(), 
+            Float.parseFloat(employeeSalary.getText()),
+            Utils.buildDate(hireDate.getText()).orElse(null) ); //TODO in else metti oggi
+        employeeName.clear();
+        employeeSurname.clear();
+        employeeCF.clear();
+        employeeSalary.clear();
+        hireDate.clear();
+    }
+
+    @FXML
+    void addSupplierOnClick(ActionEvent event) {
+        features.addSupplier(supplierName.getText());
+        supplierName.clear();
+    }
+
+    @FXML
+    void addTreatmentOnClick(ActionEvent event) {
 
     }
-    //TODO: aggiungere comandi addFornitoreOnClick
+
+    @FXML
+    void applyDiscountOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewBestSellersOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCarePlanOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewEmployeesOnShiftOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewMoreTreatedOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewNextShiftOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewProductsOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewSuppliersOnClick(ActionEvent event) {
+
+    }
 
 }
-
-
